ccf7e21ba3bd2d06d5adfdd2e9ca1b3fb20ae26a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-09-17 23:20:22 +0800

    inst*->basic-block*

--- ch3/g1.rkt
+++ ch3/g1.rkt
@@ -30,14 +30,38 @@
       [`(brz ,r ,t) (hash-set! jump-target* t #t)]
       [`(br ,t) (hash-set! jump-target* t #t)]
       [else (void)]))
-  ; first is leader
-  (cons #t
-        (map (λ (i)
-               (match i
-                 [`(tag ,name)
-                  (hash-ref jump-target* name #f)]
-                 [else #f]))
-             (cdr inst*))))
+  (map (λ (i)
+         (match i
+           [`(tag ,name)
+            (hash-ref jump-target* name #f)]
+           [else #f]))
+       (cdr inst*)))
+
+(struct basic-block
+  (inst*)
+  #:methods gen:custom-write
+  [(define (write-proc bb port mode)
+     (fprintf port "basic block:~n")
+     (for ([inst (basic-block-inst* bb)])
+       (fprintf port "\t~a~n" inst)))]
+  #:mutable
+  #:transparent)
+
+(define (basic-block-push! bb inst)
+  (set-basic-block-inst*! bb (append (basic-block-inst* bb) (list inst))))
+
+(define (inst*->basic-block* inst*)
+  (define block* '())
+  (define cur-block (basic-block (list (car inst*))))
+  (for ([inst (cdr inst*)]
+        [leader? (inst*->leader* inst*)])
+    (if leader?
+        (begin
+          (set! block* (append block* (list cur-block)))
+          (set! cur-block (basic-block '()))
+          (basic-block-push! cur-block inst))
+        (basic-block-push! cur-block inst)))
+  (append block* (list cur-block)))
 
 (define test-prog
   '((load r0 1)
@@ -49,7 +73,4 @@
     (brz r1 bar)
     (halt)))
 
-(inst*->leader* test-prog)
-
-(for ([inst test-prog])
-  (displayln (inst->string inst)))
+(inst*->basic-block* test-prog)
